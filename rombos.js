c76f772fcd8366eca6078151cdfdedd1e4de98ba
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2025-05-28 11:53:11 -0300

    Update rombos.js

--- rombos.js
+++ rombos.js
@@ -4,13 +4,14 @@ class rombos{
         this.x = pX;
         this.y = pY;
         this.ancho = 20;
+        this.tercera = random(0,50);
         /*paleta = paleta;*/
   }
   dibujar(){
     push();
     translate(43,63);
     noStroke();
-    fill(0,0,random([0,100]));
+    fill(0,0,random([0,100,this.tercera]));
     quad(this.x,this.y,this.x+this.ancho,this.y-this.ancho,this.x+this.ancho*2,this.y,this.x+this.ancho,this.y+this.ancho);
     pop();
   }
