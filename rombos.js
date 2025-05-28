4c0c8a96d85cb97f3a8f344c5db28241f2e53884
Franco D'Angelo <100707867+franciscdaydream@users.noreply.github.com>
2025-05-28 00:22:57 -0300

    Update rombos.js

--- rombos.js
+++ rombos.js
@@ -3,12 +3,12 @@ class rombos{
   constructor(pX,pY/*,paleta*/){
         this.x = pX;
         this.y = pY;
-        this.ancho = 16;
+        this.ancho = 20;
         /*paleta = paleta;*/
   }
   dibujar(){
     push();
-    translate(47,63);
+    translate(43,63);
     noStroke();
     fill(0,0,100);
     quad(this.x,this.y,this.x+this.ancho,this.y-this.ancho,this.x+this.ancho*2,this.y,this.x+this.ancho,this.y+this.ancho);
